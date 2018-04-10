d10f713f602b558ea33f40d1397ee87ff84010ab
crisrivlop <crisrivlop@gmail.com>
2018-04-10 05:28:24 -0600

    tb flotante

--- normalizador.sv
+++ normalizador.sv
@@ -3,8 +3,16 @@ module normalizador #(parameter E = 8, parameter M = 8)
 	 output logic[M-1:0] mantisa_salida, output logic[E-1:0] exponente_salida,output exp_cout);
 	 
 	 logic[M:0] mantisa_tmp;
+	 logic[M:0] shift_value;
+	 
 	 sumador_simple #(E) sum_exp(exponente_entrada,0,cout_mantisa_entrada, exponente_salida ,exp_cout);
-	 parameterizable_shift_right #(M+1) shiftrightdut(mantisa_entrada,cout_mantisa_entrada,mantisa_tmp);
+	 
+	 
+	 assign shift_value[M:1] = 0;
+	 assign shift_value[0] = cout_mantisa_entrada;
+	 
+	 parameterizable_shift_right #(M+1) shiftrightdut(mantisa_entrada,shift_value,mantisa_tmp);
+	 
 	 assign mantisa_salida = mantisa_tmp[M-1:0];
 	 
 	 
--- sumador_flotante.sv
+++ sumador_flotante.sv
@@ -1,20 +1,26 @@
-module sumador_flotante #(parameter E = 4, parameter M = 8)
-	(input logic[E+M-1:0] a, b, 
-	output logic[E+M-1:0] s, 
+module sumador_flotante #(parameter E = 8, parameter M = 23)
+	(input logic[E+M:0] a, b, 
+	output logic[E+M:0] s, 
 	output logic exp_cout);
 	
 	logic[E-1:0] exp_mayor,exp_menor,not_exp_menor,diff_exp;
 	logic[M-1:0] mant_mayor,mant_menor;
 	logic[M:0] suma_mant;
 	logic exp_cout1,mant_cout;
-	comparador_exponentes #(E) comp_exp_dut(a[E+M-2:M],b[E+M-2:M],exp_mayor,exp_menor); 
+	comparador_exponentes #(E) comp_exp_dut(a[E+M:M],b[E+M:M],exp_mayor,exp_menor); 
 	comparador_mantisas #(M) comp_mant_dut(a[M-1:0],b[M-1:0],mant_menor,mant_mayor);
 	
 	parameterizable_not #(E) not_exp_dut(exp_menor,not_exp_menor);
 	sumador_simple #(E) sum_exp_dut(exp_mayor,not_exp_menor,1, diff_exp,exp_cout1);
-	suma_mantisas #(M) suma_mant_dut(mant_menor,mant_mayor,diff_exp, suma_mant,mant_cout);
 	
-	normalizador #(E,M) normalizador_dut(suma_mant,exp_mayor, mant_cout, s[M-1:0], s[E+M-2:M],exp_cout);
+	logic[M:0] res_exp;
+	
+	assign res_exp[M:E] = 0;
+	assign res_exp[E-1:0] = diff_exp;
+	
+	suma_mantisas #(M) suma_mant_dut(mant_menor,mant_mayor,res_exp, suma_mant,mant_cout);
+	
+	normalizador #(E,M) normalizador_dut(suma_mant,exp_mayor, mant_cout, s[M-1:0], s[E+M:M],exp_cout);
 	
 	
 endmodule
